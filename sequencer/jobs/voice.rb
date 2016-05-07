25237070b3eedb4b05bb8390222c585fe2b17885
Hirofumi Wakasugi <baenej@gmail.com>
2016-05-07 16:42:43 +0900

    Optimize for Libratone ZIPP

--- sequencer/jobs/voice.rb
+++ sequencer/jobs/voice.rb
@@ -3,9 +3,9 @@ require 'nilsteps'
 
 class Voice < Rukawa::Job
   def run
-    seq = Nilsteps::Sequencer.new(bpm: 133, length: 64, osc_port: 22222)
+    seq = Nilsteps::Sequencer.new(bpm: 133, length: 32, osc_port: 22222)
     seq.setup_steps(0 => 'D2')
-    seq.play(4)
+    seq.play(8)
   end
 end
 
